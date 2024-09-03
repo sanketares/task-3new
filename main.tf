9f83a8ffc68a72215711e77432cfe4243ee677fe
sanketares <sanketares@gmail.com>
2024-09-02 23:46:14 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 
 resource "aws_instance" "my_instance_new_1" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-01fb4de0e9f8f22a7"
   instance_type = "t2.micro"           
   tags = {
     Name = "MyEC2Instance-newrduudu"
