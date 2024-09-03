b3738bbc19f07835860ec34cd7d3031d7412edce
sanket406 <sanketares@gmail.com>
2024-09-03 00:13:42 -0700

    changes

--- main.tf
+++ main.tf
@@ -1,17 +1,42 @@
 provider "aws" {
-  region = "us-west-2"  # Change this to your desired region
+  region = "us-east-1"
+}
+
+resource "aws_instance" "sanket" {
+  instance_type = "t2.micro"
+  ami           = "ami-053b0d53c279acc90"
+  
 }
 
 
-resource "aws_instance" "example_testing" {
-  ami           = "ami-01fb4de0e9f8f22a7"
-  instance_type = "t2.microoo"
 
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "sanket-s3-demo-xyz"
+}
+
+resource "aws_dynamodb_table" "terraform_lock1" {
+  name         = "terraform-lock1"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 
+resource "aws_instance" "santest" {
+  instance_type = "t2.micro"
+  ami           = "ami-053b0d53c279acc90"
+}
 
 
-
+resource "aws_instance" "sanket_ins" {
+  instance_type = "t2.micro"
+  ami           = "ami-053b0d53c279acc90"
+  tags = {
+    Name = "sanket"
+  }
+}
 
 
