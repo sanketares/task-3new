3de4a6340cc5cf58999f27e5bafd9edfb1cfeb41
sanket406 <sanketares@gmail.com>
2024-09-03 00:17:14 -0700

    change

--- main.tf
+++ main.tf
@@ -14,15 +14,7 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "sanket-s3-demo-xyz"
 }
 
-resource "aws_dynamodb_table" "terraform_lock1" {
-  name         = "terraform-lock1"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+
 
 
 resource "aws_instance" "santest" {
